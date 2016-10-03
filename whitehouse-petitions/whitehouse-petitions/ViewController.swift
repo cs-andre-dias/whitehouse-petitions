2b819c63da9e7e3f94c3301d0a779036d64a0a40
Andre Dias <andre.dias@concrete.com.br>
2016-10-03 11:15:18 -0300

    Adiciona navigation controller, tab bar controller, e a lib swiftyJson

--- whitehouse-petitions/whitehouse-petitions/ViewController.swift
+++ whitehouse-petitions/whitehouse-petitions/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var petitions = [[String: String]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        return cell
+    }
+    
+    
 }
 
